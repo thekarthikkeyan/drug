0b6896ed4743131841260f94ffe4fe9fb4ff633b
thekarthikkeyan <85353666+thekarthikkeyan@users.noreply.github.com>
2023-01-04 19:49:51 -0800

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -35,8 +35,8 @@ running = False
 cache = None
 qcache = None
 chat_log = None
-botname = 'PHARM-AI'
-username = 'PHARMAI'
+botname = 'Harshit Ethic'
+username = 'harshitethic_bot'
 # Max chat log length (A token is about 4 letters and max tokens is 2048)
 max = int(3000)
 
